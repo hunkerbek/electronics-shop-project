ea34c08979654eb641aa534a160a90eaa624af9f
hunke <hunker.90@mail.ru>
2023-09-14 14:55:30 +0300

    add homework2

--- src/item.py
+++ src/item.py
@@ -1,3 +1,8 @@
+import csv
+import os.path
+
+
+# from src.csv_errors import InstantiateCSVError
 class Item:
     """
     Класс для представления товара в магазине.
@@ -17,6 +22,35 @@ class Item:
         self.price = price
         self.quantity = quantity
         Item.all.append(self)
+
+    @property
+    def name(self):
+        """Геттер для атрибута name"""
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        """Сеттер для name, с проверкой длины наименования товара"""
+        if len(name) <= 10:
+            self.__name = name
+        else:
+            self.__name = name[:10]
+
+    @classmethod
+    def instantiate_from_csv(cls, file_name):
+        """класс-метод, инициализирующий экземпляры класса `Item` данными
+         из файла _src/items.csv_"""
+
+        with open(file_name, encoding='windows-1251') as csvfile:
+            reader = csv.DictReader(csvfile)
+            cls.all.clear()
+            for row in reader:
+                cls(row['name'], float(row['price']), int(row['quantity']))
+
+    @staticmethod
+    def string_to_number(string):
+        """статический метод, возвращающий число из числа-строки"""
+        return int(float(string))
 
     def calculate_total_price(self) -> float:
         """
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,17 +1,18 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
 
 from src.item import Item
 
-def test_calculate_total_price():
 
+def test_calculate_total_price():
     item1 = Item("Смартфон", 20000, 20)
     item2 = Item("Ноутбук", 10000, 5)
 
     assert (item1.calculate_total_price()) == 400000
     assert (item2.calculate_total_price()) == 50000
 
-def test_apply_discount():
 
+def test_apply_discount():
     item1 = Item("Смартфон", 20000, 20)
     item2 = Item("Ноутбук", 10000, 5)
 
@@ -23,4 +24,21 @@ def test_apply_discount():
     assert item1.price == 16000.0
     assert item2.price == 8000
 
-    #assert(Item.all)  # [<__main__.Item object at 0x000001EC6250C690>, <__main__.Item object at 0x000001EC6250C6D0>]
+
+def test_instantiate_from_csv():
+    Item.instantiate_from_csv('src/items.csv')
+    assert len(Item.all) == 5
+
+
+def test_name_prop():
+    item1 = Item("",0,0)
+    item1.name = "Смартфон"
+    assert item1.name == "Смартфон"
+    item1.name = "СмартфонСмартфон"
+    assert item1.name == "СмартфонСм"
+
+
+def test_string_to_number():
+    assert Item.string_to_number('5') == 5
+    assert Item.string_to_number('5.0') == 5
+    assert Item.string_to_number('5.5') == 5
